1911d5b0ef582448a69f16fc9149590b96aa0a66
Tomas Deingruber <deingruber.tomas@gmail.com>
2021-04-15 00:29:19 +0200

    Implemented parsing

--- src/main.cc
+++ src/main.cc
@@ -29,7 +29,7 @@ int main()
     std::regex re_circle_ = std::regex("^CIRCLE\\s(%|PX)\\s(\\d+)\\s(\\d+)\\s(\\d+)(:?\\s\\{(.*)\\})?$", std::regex::ECMAScript);
     [[maybe_unused]] bool matched = std::regex_match(s, match, re_circle_);
 
-    std::shared_ptr<paint::Command> command = p.ParseLine(s);
+    //std::shared_ptr<paint::Command> command = p.ParseLine(s);
 
     std::vector<std::pair<std::string, std::string>> args;
     //s = "border-color: {r: 0, g: 10, b: 255}, fill-color: {r: 0, g: 10, b: 255}, border-width: 10";
@@ -51,4 +51,6 @@ int main()
     std::cout << r << ", " << g << ", " << b << std::endl;
 
     std::cout << re_param_color_.mark_count() << ", " << match.size() << std::endl;
+
+    paint::Parser::ParseLine("CIRCLE PX 1 2 3 {fill-color: {r: 0, g: 5, b: 255}}");
 }
