5494dca16a4e43fa9bf7c74f4160a0deeed8fd25
Tomas Deingruber <deingruber.tomas@gmail.com>
2021-04-15 23:47:49 +0200

    forgotten merge

--- inc/image.h
+++ inc/image.h
@@ -19,20 +19,12 @@ namespace paint
         virtual void GenerateMetadata() = 0;
 
     protected:
-<<<<<<< HEAD
-        // std::unique_ptr<ImageData<T>> image_data_;
-        // std::deque<std::unique_ptr<ImageData<T>>> image_data_history_;
-
-    private:
-        File file_;
-=======
         File file_;
 
     private:
         std::unique_ptr<DataPixels> image_data_;
         std::deque<std::unique_ptr<DataPixels>> image_data_history_;
         Painter painter_;
->>>>>>> painter
     };
 }
 #endif // PAINT_INC_IMAGE_H_
