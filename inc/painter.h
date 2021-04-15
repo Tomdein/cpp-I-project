d1097a920ce77960c85ea10dc8ea84b13e0e5746
Tomas Deingruber <deingruber.tomas@gmail.com>
2021-04-15 15:28:44 +0200

    changed painter to interface

--- inc/painter.h
+++ inc/painter.h
@@ -3,41 +3,46 @@
 
 #include <memory>
 
+#include "color.h"
 #include "image_data.h"
 
 namespace paint
 {
-    template <class T>
     class Painter
     {
     public:
         Painter() = default;
 
-        void SetNextColor(std::shared_ptr<Color> &color) = 0;
+        virtual void SetData();
+
+        virtual void SetNextColor(std::shared_ptr<Color> &color) = 0;
 
         // Move to img?
         // virtual void LoadImage() = 0;
         // virtual void SaveImage() = 0;
         // virtual void GenerateMetadata() = 0;
 
-        void DrawLine();
-        void DrawCircle();
-        void DrawBucket();
+        virtual void DrawLine() = 0;
+        virtual void DrawCircle() = 0;
+        virtual void DrawBucket() = 0;
 
         // Crop, Resize and Rotate modifies the headers
-        void Crop();
-        void Resize();
-        void Rotate();
+        virtual void Crop() = 0;
+        virtual void Resize() = 0;
+        virtual void Rotate() = 0;
 
         // InvertColors and ConvertToGrayscale *CAN* modify the headers
-        void InvertColors();
-        void ConvertToGrayscale();
+        virtual void InvertColors() = 0;
+        virtual void ConvertToGrayscale() = 0;
 
         // Undo and Redo *CAN* modify the headers
-        void Undo();
-        void Redo();
+        virtual void Undo() = 0;
+        virtual void Redo() = 0;
 
-        void SetNextColor(std::weak_ptr<Color> &color) final { image_data_.get()->SetNextCommandColor(color); }
+        // void SetNextColor(std::weak_ptr<Color> &color) final { image_data_.get()->SetNextCommandColor(color); }
+
+    private:
+        std::unique_ptr<Color> color_format_;
     };
 }
 
