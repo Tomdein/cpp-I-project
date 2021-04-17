c539910cdb490c42004325eebd161376d8c383c7
Tomas Deingruber <deingruber.tomas@gmail.com>
2021-04-17 21:43:55 +0200

    cleared main

--- src/main.cc
+++ src/main.cc
@@ -1,16 +1,4 @@
 #include <iostream>
-#include <string>
-#include <memory>
-#include <regex>
-#include <random>
-
-#include <typeinfo>
-
-#include "parser.h"
-#include "colors.h"
-#include "paint.h"
-
-std::regex re_param_color_ = std::regex("^r:\\s(\\d{1,3}),\\sg:\\s(\\d{1,3}),\\sb:\\s(\\d{1,3})$", std::regex::ECMAScript);
 
 int main()
 {
