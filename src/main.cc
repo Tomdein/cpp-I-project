6013eeb2a4cd08b5932363f707e40c904e5f736d
Tomas Deingruber <deingruber.tomas@gmail.com>
2021-04-19 01:40:21 +0200

    does a hang

--- src/main.cc
+++ src/main.cc
@@ -15,7 +15,7 @@ int main()
     //image.painter.Crop(paint::PointPX(0, 10), paint::PointPX(5, 15)); // Error
     //image.painter.Resize(paint::PointPX(36, 24)); //terminate called without an active exception. Aborted
     //image.painter.Resize(paint::PointPX(36, 24)); // Shifted by a pixel up
-    //image.painter.DrawBucket(paint::PointPX(0, 0), std::optional<std::shared_ptr<paint::Color>>(new paint::ColorRGB888(255, 0, 0))); //Changes only one pixel to BLUE?
+    image.painter.DrawBucket(paint::PointPX(0, 0), std::optional<std::shared_ptr<paint::Color>>(new paint::ColorRGB888(255, 0, 0))); //Changes only one pixel to BLUE?
     image.painter.InvertColors();
     image.SaveImage();
 
